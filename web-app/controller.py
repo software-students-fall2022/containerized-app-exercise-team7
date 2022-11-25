6f88ce08fee358c0d6b97e76c5f8f29eb9f8c59b
Spectraorder <2421036333@qq.com>
2022-11-25 18:49:51 -0500

    front end bootstrap

--- web-app/controller.py
+++ web-app/controller.py
@@ -4,14 +4,17 @@ from unicodedata import name
 from dotenv import dotenv_values
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+from flask_bootstrap import Bootstrap
 
 import pymongo
 import datetime
 import sys
+# import db as translator
 import speech_recognition as sr
 
 # instantiate the app
 app = Flask(__name__)
+bootstrap=Bootstrap(app)
 # # load credentials and configuration options from .env file
 # # if you do not yet have a file named .env, make one based on the template in env.example
 # config = dotenv_values(".env")
@@ -46,31 +49,34 @@ app = Flask(__name__)
 #route for homepage 
 #Takes in a audio file and display the transcript
 @app.route('/', methods = ["GET", "POST"])
-def index():
+def home():
     transcript = ""
     """
     Route for the home page
     """
     if request.method == "POST":
-        print("FORM DATA RECEIVED")
+        # print("FORM DATA RECEIVED")
 
         if "file" not in request.files:
-            return redirect(request.url)
+            return render_template('home.html', error=True)
         
         file = request.files["file"]
         if file.filename == "":
-            return redirect(request.url)
+            return render_template('home.html', error=True)
 
         if file:
-            recognizer = sr.Recognizer()
-            audioFile = sr.AudioFile(file)
-            with audioFile as source:
-                data = recognizer.record(source)
-            transcript = recognizer.recognize_google(data, key=None)
-            print(transcript)
+            try:
+                recognizer = sr.Recognizer()
+                audioFile = sr.AudioFile(file)
+                with audioFile as source:
+                    data = recognizer.record(source)
+                transcript = recognizer.recognize_google(data, key=None)
+            except:
+                return render_template('home.html', error=True)
+            return render_template('home.html', transcript=transcript, cont=True)
 
         
-    return render_template('index.html', transcript = transcript) # render the home template
+    return render_template('home.html') # render the home template
 
 def get_db():
     client = MongoClient(host='test_mongodb',
