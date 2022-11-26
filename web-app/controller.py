dda8dfe1940c4f61b890887402b22a7dee798f8e
Spectraorder <2421036333@qq.com>
2022-11-25 20:11:42 -0500

    database initial

--- web-app/controller.py
+++ web-app/controller.py
@@ -9,12 +9,38 @@ from flask_bootstrap import Bootstrap
 import pymongo
 import datetime
 import sys
-# import db as translator
 import speech_recognition as sr
 
 # instantiate the app
 app = Flask(__name__)
 bootstrap=Bootstrap(app)
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+input_audio = myclient["input_audio"]
+lang = myclient["language"]
+
+# def get_db():
+#     client = MongoClient(host='test_mongodb',
+#                          port=27017, 
+#                          username='root', 
+#                          password='pass',
+#                          authSource="admin")
+#     db = client["lang"]
+#     return db
+
+def db_init():
+    lang.insertMany([[
+                        {"_id":1,"languages":"Chinese","code":"zh-CN"},
+                        {"_id":2,"languages":"English","code":"en"},
+                        {"_id":3,"languages":"French","code":"fr"},
+                        {"_id":4,"languages":"Italian","code":"it"},
+                        {"_id":5,"languages":"Japanese","code":"ja"},
+                        {"_id":6,"languages":"Korean","code":"ko"},
+                        {"_id":7,"languages":"Portuguese","code":"pt"},
+                        {"_id":8,"languages":"Russian","code":"ru"},
+                        {"_id":9,"languages":"Spanish","code":"es"}]
+                    ])
+
 # # load credentials and configuration options from .env file
 # # if you do not yet have a file named .env, make one based on the template in env.example
 # config = dotenv_values(".env")
@@ -23,7 +49,6 @@ bootstrap=Bootstrap(app)
 # if config['FLASK_ENV'] == 'development':
 #     # turn on debugging, if in development
 #     app.debug = True # debug mnode
-
 
 # # connect to the database
 # cxn = pymongo.MongoClient(config['MONGO_URI'], serverSelectionTimeoutMS=5000)
@@ -78,33 +103,24 @@ def home():
         
     return render_template('home.html') # render the home template
 
-def get_db():
-    client = MongoClient(host='test_mongodb',
-                         port=27017, 
-                         username='root', 
-                         password='pass',
-                         authSource="admin")
-    db = client["lang"]
-    return db
-
-@app.route('/languages', methods=['GET', 'POST'])
-def get_languagess():
-    if request.method=='GET':
-        # do something with translation maybe and voice recording
-        return render_template('languages.html')
-    if request.method=='POST':
-        db=""
-        try:
-            db = get_db()
-            languages = db.langs.find()
-            language = [{"id": langs["_id"], "lang": langs["lang"], "code": langs["code"]} for langs in languages]
-            #return render_template('languages.html', languages-languages)
-            return jsonify({"languages": language})
-        except:
-            pass
-        finally:
-            if type(db)==MongoClient:
-                db.close()
+# @app.route('/languages', methods=['GET', 'POST'])
+# def get_languagess():
+#     if request.method=='GET':
+#         # do something with translation maybe and voice recording
+#         return render_template('languages.html')
+#     if request.method=='POST':
+#         db=""
+#         try:
+#             db = get_db()
+#             languages = db.langs.find()
+#             language = [{"id": langs["_id"], "lang": langs["lang"], "code": langs["code"]} for langs in languages]
+#             #return render_template('languages.html', languages-languages)
+#             return jsonify({"languages": language})
+#         except:
+#             pass
+#         finally:
+#             if type(db)==MongoClient:
+#                 db.close()
 
 #(NOTDONE)
 #route for translating the recognized audio file input using machine learning
