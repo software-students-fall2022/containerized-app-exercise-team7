d02c3f70cab7616c5879dee3c21051fdb3eda18f
Hillary <hillarydavis@Hillarys-MBP.lan>
2022-11-27 16:32:51 -0600

    added comments to trans file for translating

--- web-app/trans.py
+++ web-app/trans.py
@@ -1,8 +1,10 @@
 from deep_translator import GoogleTranslator
 
 def trans(input,src,tgt):
+    #if src does not have any input that use "auto" functionality - this has not been implemented yer
     if src != "":
         return GoogleTranslator(source=src, target=tgt).translate(text=input)
+    #translate given the src and target, etc
     else:
         return GoogleTranslator(source='auto', target=tgt).translate(text=input)
 
