55d90d63b5cf41fb28d0c6842f924c8cbce07b9c
Leo6016 <gx324@nyu.edu>
2022-12-02 14:25:57 -0500

    get db test

--- web_app/controller.py
+++ web_app/controller.py
@@ -58,30 +58,31 @@ if config['FLASK_DEBUG'] == 'development':
 
 
 def get_db():
-    db = cxn[config['MONGO_DBNAME']]  # store a reference to the database
-    return db
+    cxn_test = pymongo.MongoClient(config['MONGO_URI'], serverSelectionTimeoutMS=5000)
+    db_test = cxn_test[config['MONGO_DBNAME']]  # store a reference to the database
+    return db_test
 
 
 def db_init():
-    cxn = pymongo.MongoClient(config['MONGO_URI'],
-                        username=config['MONGO_USER'],
-                        password=config['MONGO_PASS'],
-                        serverSelectionTimeoutMS=5000)
-    try:
-        # verify the connection works by pinging the database
-        # The ping command is cheap and does not require auth.
-        cxn.admin.command('ping')
-        db = cxn[config['MONGO_DBNAME']]  # store a reference to the database
-        # if we get here, the connection worked!
-        print(' *', 'Connected to MongoDB!')
+    # cxn = pymongo.MongoClient(config['MONGO_URI'],
+    #                     username=config['MONGO_USER'],
+    #                     password=config['MONGO_PASS'],
+    #                     serverSelectionTimeoutMS=5000)
+    # try:
+    #     # verify the connection works by pinging the database
+    #     # The ping command is cheap and does not require auth.
+    #     cxn.admin.command('ping')
+    #     db = cxn[config['MONGO_DBNAME']]  # store a reference to the database
+    #     # if we get here, the connection worked!
+    #     print(' *', 'Connected to MongoDB!')
 
-    except Exception as e:
-        # the ping command failed, so the connection is not available.
-        # render_template('error.html', error=e) # render the edit template
-        print(' *', "Failed to connect to MongoDB at", config['MONGO_URI'])
-        print('Database connection error:', e)  # debug
-    # set outside for ease
-    db.langs.delete_many([{}])
+    # except Exception as e:
+    #     # the ping command failed, so the connection is not available.
+    #     # render_template('error.html', error=e) # render the edit template
+    #     print(' *', "Failed to connect to MongoDB at", config['MONGO_URI'])
+    #     print('Database connection error:', e)  # debug
+    # # set outside for ease
+    # db.langs.delete_many([{}])
     db.langs.insert_many([{"lang": "Bulgarian", "code": "bg"},
                           {"lang": "Czech", "code": "cs"},
                           {"lang": "Danish", "code": "da"},
@@ -130,6 +131,7 @@ def home():
     """
     Route for the home page
     """
+    db.langs.delete_many([{}])
     # initalize the database with the languages that can be translated
     db_init()
     # pass database in twice for both drop down menus
