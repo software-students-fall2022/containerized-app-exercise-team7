4fbefee44b81b66e8bb6dc3b2bade0965408a4d7
Spectraorder <2421036333@qq.com>
2022-12-03 17:10:15 -0500

    back end

--- web_app/controller.py
+++ web_app/controller.py
@@ -23,44 +23,35 @@ import sys
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
-
-#myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-#input_audio = myclient["input_audio"]
-#lang = myclient["language"]
-
 # load credentials and configuration options from .env file
 # if you do not yet have a file named .env, make one based on the template in env.example
 
-def get_db():
+def get_db(num):
         # turn on debugging if in development mode
     config = dotenv_values(".env")
     if config['FLASK_DEBUG'] == 'development':
         # turn on debugging, if in development
         app.debug = True  # debug mode
-
-        # cxn = pymongo.MongoClient(config['MONGO_URI'],
-        #                     username=config['MONGO_USER'],
-        #                     password=config['MONGO_PASS'],
-        #                     serverSelectionTimeoutMS=5000)
         cxn = pymongo.MongoClient(config['MONGO_URI'], serverSelectionTimeoutMS=5000)
         try:
             # verify the connection works by pinging the database
             # The ping command is cheap and does not require auth.
             cxn.admin.command('ping')
-            db = cxn[config['MONGO_DBNAME']]  # store a reference to the database
+            if num==0:
+                db = cxn[config['MONGO_LANG_DBNAME']]  # store a reference to the database
+            else:
+                db = cxn[config['MONGO_TEXT_DBNAME']]
             # if we get here, the connection worked!
             print(' *', 'Connected to MongoDB!')
-
         except Exception as e:
             # the ping command failed, so the connection is not available.
             # render_template('error.html', error=e) # render the edit template
             print(' *', "Failed to connect to MongoDB at", config['MONGO_URI'])
             print('Database connection error:', e)  # debug
-        # set outside for ease
     return db
 
 
-def db_init(db):
+def db_lang_init(db):
     db.langs.delete_many({})
     db.langs.insert_many([{"lang": "Bulgarian", "code": "bg"},
                           {"lang": "Czech", "code": "cs"},
@@ -91,6 +82,9 @@ def db_init(db):
                           {"lang": "Chinese", "code": "zh-CN"},
                           ])
 
+def db_text_add(db, file, input_text, output_text):
+    db.hist.insert_one({})
+
 # ****************** All Routes ******************************#
 # (DONE)
 
@@ -98,16 +92,11 @@ def db_init(db):
 # Takes in a audio file and display the transcript
 @app.route('/', methods=["GET", "POST"])
 def home():
-    """
-    Route for the home page
-    """
-
-    db=get_db()
+    db=get_db(0)
     # initalize the database with the languages that can be translated
-    db_init(db)
+    db_lang_init(db)
     # pass database in twice for both drop down menus
-
-    inp = db.langs.find({})
+    # inp = db.langs.find({})
     out = db.langs.find({})
     if request.method == "POST":
         # get audio from app.js
@@ -115,6 +104,7 @@ def home():
         # save audio to audio.wav file through flask server
         with open('audio.wav', 'wb') as audio:
             f.save(audio)
+            # global file
             file = 'audio.wav'
         if file:
             # implement speech recognition
@@ -125,10 +115,10 @@ def home():
             # save the audio translation to global variable for easy accesibility
             global transcript
             transcript = recognizer.recognize_google(data, key=None)
-
-        #print('file uploaded successfully')
+            print(transcript)
     # pass database in to be read in home.html
-    return render_template('home.html', inp=inp, out=out)
+    # return render_template('home.html', inp=inp, out=out)
+    return render_template('home.html', out=out)
 
 # route for translating the recognized audio file input using machine learning
 
@@ -138,7 +128,8 @@ def translate():
     # get the options selected from input and output from home.html
     inp = "English"
     out = request.form.get('output')
-    db=get_db()
+    db=get_db(0)
+    db_text=get_db(1)
     # using the languages chosen by the user locate their doc in the database
     src = db.langs.find_one({"lang": str(inp)})
     targ = db.langs.find_one({"lang": str(out)})
@@ -147,6 +138,7 @@ def translate():
     t = targ["code"]
     # call the trans function and translate the text to language
     in_out = trans.trans(transcript, s, t)
+    # db_text_add(db_text, file, transcript, in_out)
     return render_template('translate.html', in_out=in_out, transcript=transcript)
 
 
