0b48d7f6965d128950298239c61d128e45df18db
Spectraorder <2421036333@qq.com>
2022-12-04 15:00:52 -0500

    database test committed

--- web_app/tests/test_database.py
+++ web_app/tests/test_database.py
@@ -33,16 +33,40 @@ class Test_Web_App(unittest.TestCase):
     def test_db_collection(self):
         self.setUp()
         response = self.client.get('/', follow_redirects=True)
-        db = get_db(0)
-        assert db.list_collection_names() == ["langs"]
+        db_text = get_db(1)
+        db_lang = get_db(0)
+        assert db_text.list_collection_names() == ["hist"]
+        assert db_lang.list_collection_names() == ["langs"]
 
 
     def test_db_languages(self):
         self.setUp()
         response = self.client.get('/', follow_redirects=True)
         db = get_db(0)
+        assert db.langs.find({"lang": "Bulgarian", "code": "bg"})
+        assert db.langs.find({"lang": "Czech", "code": "cs"})
+        assert db.langs.find({"lang": "Danish", "code": "da"})
+        assert db.langs.find({"lang": "German", "code": "de"})
+        assert db.langs.find({"lang": "Greek", "code": "el"})
         assert db.langs.find({"lang": "English", "code": "en"})
         assert db.langs.find({"lang": "Spanish", "code": "es"})
+        assert db.langs.find({"lang": "Estonian", "code": "et"})
+        assert db.langs.find({"lang": "Finnish", "code": "fi"})
+        assert db.langs.find({"lang": "Hungarian", "code": "hu"})
+        assert db.langs.find({"lang": "Italian", "code": "it"})
+        assert db.langs.find({"lang": "Lithuanian", "code": "lt"})
+        assert db.langs.find({"lang": "Latvian", "code": "lv"})
+        assert db.langs.find({"lang": "Dutch", "code": "nl"})
+        assert db.langs.find({"lang": "Polish", "code": "pl"})
+        assert db.langs.find({"lang": "Portuguese (Brazil)", "code": "pt-BR"})
+        assert db.langs.find({"lang": "Portuguese (Portugal)", "code": "pt-PT"})
+        assert db.langs.find({"lang": "Romanian", "code": "ro"})
+        assert db.langs.find({"lang": "Russian", "code": "ru"})
+        assert db.langs.find({"lang": "Slovak", "code": "sk"})
+        assert db.langs.find({"lang": "Slovenian", "code": "sl"})
+        assert db.langs.find({"lang": "Swedish", "code": "sv"})
+        assert db.langs.find({"lang": "Turkish", "code": "tr"})
+        assert db.langs.find({"lang": "Ukrainian", "code": "uk"})
         assert db.langs.find({"lang": "French", "code": "fr"})
         assert db.langs.find({"lang": "Japanese", "code": "ja"})
         assert db.langs.find({"lang": "Chinese", "code": "zh-CN"})
